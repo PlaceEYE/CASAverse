202ccb814b1e844d6d0fc6bc613e1829f5f38462
Seungjae Lim <seungjaelim@kaist.ac.kr>
2023-07-29 12:53:58 +0900

    FEAT: Add tesla...

--- casa/src/pages/winter.js
+++ casa/src/pages/winter.js
@@ -3,97 +3,50 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as THREE from "three";
 import { Suspense, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import SceneInit from './lib/SceneInit.js';
 
 extend({ OrbitControls });
 
-const BackGround = (props) => {
-  const texture = useLoader(THREE.TextureLoader, process.env.PUBLIC_URL + "/assets/winter.png");
-  const { scene, gl } = useThree();
-
+function App() {
   useEffect(() => {
-    if (texture) {
-      const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
-      rt.fromEquirectangularTexture(gl, texture);
-      scene.background = rt.texture;
-    }
-  }, [texture, gl, scene]);
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
 
-  return null;
-};
+    // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
 
-const Box = (props) => {
-  const ref = useRef();
-  const originalColor = "#bbff2c";
-  const hoverColor = "cyan";
-  const navigate = useNavigate();
+    let loadedModel;
+    const glftLoader = new GLTFLoader();
+    glftLoader.load(process.env.PUBLIC_URL + "/assets/scene.gltf", (gltfScene) => {
+      loadedModel = gltfScene;
+      // console.log(loadedModel);
 
-  useFrame((state) => {
-    ref.current.rotation.x += 0.01;
-    ref.current.rotation.y += 0.01;
-  });
+      gltfScene.scene.rotation.y = Math.PI / 8;
+      gltfScene.scene.position.y = 3;
+      gltfScene.scene.scale.set(0.1, 0.1, 0.1);
+      test.scene.add(gltfScene.scene);
+    });
 
-  const handlePointerEnter = (e) => {
-    e.object.scale.x = 1.5;
-    e.object.scale.y = 1.5;
-    e.object.scale.z = 1.5;
-    e.object.material.color.set(hoverColor);
-  };
-
-  const handlePointerLeave = (e) => {
-    e.object.scale.x = 1;
-    e.object.scale.y = 1;
-    e.object.scale.z = 1;
-    e.object.material.color.set(originalColor);
-  };
-
-  const handleClick = () => {
-    navigate("/CASAverse");
-  };
+    const animate = () => {
+      if (loadedModel) {
+        loadedModel.scene.rotation.x += 0.01;
+        loadedModel.scene.rotation.y += 0.01;
+        loadedModel.scene.rotation.z += 0.01;
+      }
+      requestAnimationFrame(animate);
+    };
+    animate();
+  }, []);
 
   return (
-    <mesh
-      ref={ref}
-      {...props}
-      castShadow
-      receiveShadow
-      onPointerEnter={handlePointerEnter}
-      onPointerLeave={handlePointerLeave}
-      onClick={handleClick}
-    >
-      <boxBufferGeometry attach="geometry" />
-      <meshPhysicalMaterial attach="material" metalness={0.5} roughness={0.5} color={originalColor} />
-    </mesh>
-  );
-};
-
-const Sun = (props) => {
-  return (
-    <mesh {...props}>
-      <pointLight castShadow />
-    </mesh>
-  );
-};
-
-const Orbit = () => {
-  const { camera, gl } = useThree();
-  return <orbitControls args={[camera, gl.domElement]} />;
-};
-
-function Title() {
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas shadows style={{ background: "black" }} camera={{ position: [3, 3, 3] }}>
-        <ambientLight intensity={0.2} />
-        <Box />
-        <Sun position={[0, 3, 0]} />
-        <pointLight />
-        <Orbit />
-        <Suspense fallback={null}>
-          <BackGround />
-        </Suspense>
-      </Canvas>
+    <div>
+      <canvas id="myThreeJsCanvas" />
     </div>
   );
 }
 
-export default Title;
+export default App;
